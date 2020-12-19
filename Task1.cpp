481c9c37dd7fce776a34dd4d530052e695a7bd3f
Rustam <afaritovich@mail.ru>
2020-12-19 15:01:52 +0700

    add: solved task-1

---
+++ Task1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+class RaiiThread{
+private:
+    std::thread thr_;
+
+public:
+    template<class Function, class... Args>
+    RaiiThread(Function&& fn, Args&&... args) : thr_(fn, args...){}
+
+    ~RaiiThread(){
+        thr_.join();
+        std::cout << "Yeeah, baby\n";
+    }
+};
+
+void check(){
+    std::cout << "run void check()\n";
+    return;
+}
+
+int main()
+{
+    RaiiThread{check};
+    return 0;
+}
